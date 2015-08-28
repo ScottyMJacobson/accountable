e043599f76b4ad70002e34f34434be3c97795f7c
Scotty Jacobson <ScottyMJacobson@gmail.com>
2015-08-28 13:31:18 -0400

    Commitment models - use auth user from settings instead of default
    Also fixed error where the owner of a dailysnapshot was a user instead of a commitmentprofile

--- commitments/models.py
+++ commitments/models.py
@@ -1,11 +1,12 @@
 from django.db import models
+from django.conf import settings
 from django.contrib.auth.models import User
 import datetime
 
 # The profile of a user, which contains the commitments they have created as well as their progress on each
 class CommitmentProfile(models.Model):
     # Optional because there are users who will be observers who don't have a commitment profile...
-    user = models.OneToOneField(User, blank=True, null=True)
+    user = models.OneToOneField(settings.AUTH_USER_MODEL, blank=True, null=True)
 
 
 # A single commitment, with a name and description (and eventually, frequency, alert settings, etc)
@@ -20,7 +21,7 @@ class Commitment(models.Model):
 # A representation of one day's worth of commitments and how each has done
 class CommitmentDailySnapshot(models.Model):
     date = models.DateField()
-    owner = models.ForeignKey(User)
+    owner = models.ForeignKey(CommitmentProfile)
 
 
 # The progress of one specific commitment on one specific day
