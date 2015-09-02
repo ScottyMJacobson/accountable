6bc7cc68ca12b4cf20ee7ca655cf500635b701fa
Scotty Jacobson <ScottyMJacobson@gmail.com>
2015-09-02 17:00:35 -0400

    view for commitment_profile

--- commitments/views.py
+++ commitments/views.py
@@ -1,3 +1,31 @@
-from django.shortcuts import render
+from rest_framework import status
+from rest_framework.decorators import api_view, permission_classes
 
-# Create your views here.
+from rest_framework.permissions import IsAuthenticated
+
+from rest_framework.response import Response
+
+from commitments.models import CommitmentProfile, \
+                                        Commitment, \
+                                        CommitmentDailySnapshot, \
+                                        CommitmentStatus
+
+from commitments.serializers import CommitmentDailySnapshotSerializer,\
+                                        CommitmentProfileSerializer, \
+                                        CommitmentSerializer, \
+                                        CommitmentStatusSerializer
+
+
+
+@api_view(['GET'])
+@permission_classes((IsAuthenticated,))
+def commitment_profile(request, format=None):
+    """
+    Get the commitment profile for the currently authenticated
+    user
+    """
+    if request.method == 'GET':
+        current_profile = request.user.commitmentprofile
+        serializer = CommitmentProfileSerializer(current_profile)
+        return Response(serializer.data)
+
