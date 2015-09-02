339ca08e11d181553d789e0467d5ce434b143761
Scotty Jacobson <ScottyMJacobson@gmail.com>
2015-09-02 12:49:31 -0400

    break out tests into separate files for models and serializers

--- commitments/test_models.py
+++ commitments/test_models.py
@@ -1,7 +1,7 @@
 from django.test import TestCase
 from django.contrib.auth import get_user_model
+
 from commitments.models import *
-from commitments.serializers import CommitmentSerializer
 
 from django.utils import timezone
 import datetime
@@ -74,28 +74,6 @@ class CommitmentSnapshotTestCase(TestCase):
         self.assertEqual(updated_status.time_accomplished, time_accomplished)
 
     
-class CommitmentSerializerTestCase(TestCase):
-    def setUp(self):
-        self.username = "test_user"
-        self.email = "test@email.com"
-        self.password = "test_password"
-        self.user =  get_user_model().objects.create_user(
-                username = self.username,
-                email = self.email,
-                password = self.password,
-            )
-        self.dummy_commitment_name = 'wash face'
-        self.dummy_commitment_description = 'wash your face every night'
-        my_commitment_profile = self.user.commitmentprofile
-        my_commitment_profile.register_commitment(self.dummy_commitment_name, 
-            self.dummy_commitment_description)
-        self.dummy_commitment = self.user.commitmentprofile.get_active_commitments()[0]
-
-    def test_commitment_serializer(self):
-        self.serializer = CommitmentSerializer(self.dummy_commitment)
-        self.assertEqual(self.serializer.data['name'], self.dummy_commitment_name)
-        self.assertEqual(self.serializer.data['owner'], self.user.id)
-
 
 
 
