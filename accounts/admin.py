7ed7a03f918addf69adc605b30c365fd3f140c7c
Scotty Jacobson <ScottyMJacobson@gmail.com>
2015-09-03 12:17:24 -0400

    add User to Admin app

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,5 +1,13 @@
 from django.contrib import admin
 from accounts.models import User
+from commitments.models import CommitmentProfile
 
-admin.site.register(User)
- 
+class CommitmentProfileInline(admin.StackedInline):
+    model = CommitmentProfile
+
+class UserAdmin(admin.ModelAdmin):
+    inlines = (CommitmentProfileInline,)
+    fields = ('email', 'username', 'is_active',
+        'is_admin', 'is_staff')
+
+admin.site.register(User, UserAdmin)
