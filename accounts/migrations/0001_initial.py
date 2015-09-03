4dd8fed3acc01870154fcf8373070e125926ac6f
Scotty Jacobson <ScottyMJacobson@gmail.com>
2015-09-03 00:41:59 -0400

    blew up migrations; new shiny all-encompassing migrations for all data models in 'accounts' and 'commitments'

--- accounts/migrations/0001_initial.py
+++ accounts/migrations/0001_initial.py
@@ -2,11 +2,13 @@
 from __future__ import unicode_literals
 
 from django.db import models, migrations
+import django.contrib.auth.models
 
 
 class Migration(migrations.Migration):
 
     dependencies = [
+        ('auth', '0006_require_contenttypes_0002'),
     ]
 
     operations = [
@@ -15,14 +17,22 @@ class Migration(migrations.Migration):
             fields=[
                 ('id', models.AutoField(primary_key=True, auto_created=True, verbose_name='ID', serialize=False)),
                 ('password', models.CharField(max_length=128, verbose_name='password')),
-                ('last_login', models.DateTimeField(blank=True, verbose_name='last login', null=True)),
-                ('email', models.EmailField(db_index=True, max_length=254, unique=True, verbose_name='email address')),
-                ('joined', models.DateTimeField(auto_now_add=True)),
+                ('last_login', models.DateTimeField(null=True, blank=True, verbose_name='last login')),
+                ('is_superuser', models.BooleanField(help_text='Designates that this user has all permissions without explicitly assigning them.', default=False, verbose_name='superuser status')),
+                ('email', models.EmailField(max_length=254, db_index=True, unique=True, verbose_name='email address')),
+                ('username', models.CharField(max_length=30, unique=True, verbose_name='username')),
+                ('date_joined', models.DateTimeField(auto_now_add=True)),
                 ('is_active', models.BooleanField(default=True)),
                 ('is_admin', models.BooleanField(default=False)),
+                ('is_staff', models.BooleanField(default=False)),
+                ('groups', models.ManyToManyField(related_name='user_set', verbose_name='groups', blank=True, help_text='The groups this user belongs to. A user will get all permissions granted to each of their groups.', to='auth.Group', related_query_name='user')),
+                ('user_permissions', models.ManyToManyField(related_name='user_set', verbose_name='user permissions', blank=True, help_text='Specific permissions for this user.', to='auth.Permission', related_query_name='user')),
             ],
             options={
                 'abstract': False,
             },
+            managers=[
+                ('objects', django.contrib.auth.models.UserManager()),
+            ],
         ),
     ]
--- commitments/migrations/0001_initial.py
+++ commitments/migrations/0001_initial.py
@@ -3,6 +3,7 @@ from __future__ import unicode_literals
 
 from django.db import models, migrations
 from django.conf import settings
+import annoying.fields
 import datetime
 
 
@@ -17,7 +18,8 @@ class Migration(migrations.Migration):
             name='Commitment',
             fields=[
                 ('id', models.AutoField(primary_key=True, auto_created=True, verbose_name='ID', serialize=False)),
-                ('created', models.DateTimeField(blank=True, default=datetime.datetime.now)),
+                ('created', models.DateTimeField(auto_now_add=True)),
+                ('modified', models.DateTimeField(auto_now=True)),
                 ('name', models.CharField(max_length=50)),
                 ('description', models.CharField(max_length=200)),
                 ('due_time', models.TimeField(default=datetime.time(23, 59))),
@@ -27,21 +29,22 @@ class Migration(migrations.Migration):
             name='CommitmentDailySnapshot',
             fields=[
                 ('id', models.AutoField(primary_key=True, auto_created=True, verbose_name='ID', serialize=False)),
-                ('date', models.DateField()),
+                ('date', models.DateField(default=datetime.date.today)),
             ],
         ),
         migrations.CreateModel(
             name='CommitmentProfile',
             fields=[
                 ('id', models.AutoField(primary_key=True, auto_created=True, verbose_name='ID', serialize=False)),
-                ('user', models.OneToOneField(blank=True, to=settings.AUTH_USER_MODEL, null=True)),
+                ('user', annoying.fields.AutoOneToOneField(to=settings.AUTH_USER_MODEL)),
             ],
         ),
         migrations.CreateModel(
             name='CommitmentStatus',
             fields=[
                 ('id', models.AutoField(primary_key=True, auto_created=True, verbose_name='ID', serialize=False)),
-                ('time_accomplished', models.DateTimeField(blank=True, null=True)),
+                ('time_accomplished', models.DateTimeField(null=True, blank=True)),
+                ('comment', models.CharField(null=True, max_length=140, blank=True)),
                 ('commitment', models.ForeignKey(to='commitments.Commitment')),
                 ('parent_snapshot', models.ForeignKey(to='commitments.CommitmentDailySnapshot')),
             ],
