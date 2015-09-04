9153b290cef2248b365c00980250791c8ef9bbb0
Scotty Jacobson <ScottyMJacobson@gmail.com>
2015-09-04 14:18:51 -0400

    fix typo in commitments/urls (missing comma)

--- commitments/urls.py
+++ commitments/urls.py
@@ -6,8 +6,8 @@ from commitments import views
 
 urlpatterns = [
     url(r'^profile/$', views.commitment_profile),
-    url(r'^profile/commitments/$', views.commitments_list)
-    url(r'^profile/commitments/(?P<pk>[0-9]+)$', views.commitments_detail)
+    url(r'^profile/commitments/$', views.commitments_list),
+    url(r'^profile/commitments/(?P<pk>[0-9]+)$', views.commitments_detail),
 ]
 
 urlpatterns = format_suffix_patterns(urlpatterns)
